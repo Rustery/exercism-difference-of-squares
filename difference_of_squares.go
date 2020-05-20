a19c05800de389894169052925df5bbb4b826222
Rustam <rustery@gmail.com>
2020-05-20 12:16:58 +0300

    removed loops for sum calculation

--- difference_of_squares.go
+++ difference_of_squares.go
@@ -1,24 +1,15 @@
 // Package diffsquares is Exercism.io exercise
 package diffsquares
 
-import "math"
-
 // SquareOfSum — get square of the sum of the first N natural numbers
-func SquareOfSum(n int) int{
-	sum := 0
-	for i := 0; i <= n; i++ {
-		sum += i
-	}
-	return int(math.Pow(float64(sum), 2))
+func SquareOfSum(n int) int {
+	sum := ((n * n) + n) / 2
+	return sum * sum
 }
 
 // SumOfSquares — get sum of the squares of the first N natural numbers
 func SumOfSquares(n int) int {
-	sum := 0
-	for i := 0; i <= n; i++ {
-		sum += int(math.Pow(float64(i), 2))
-	}
-	return sum
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 // Difference — get the difference between the square of the sum of the first N natural numbers
